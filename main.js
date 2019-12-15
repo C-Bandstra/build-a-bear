f29855d0375a3afd9c519bc182be52aed9bd2d61
Mandy Alford <mandyalford@gmail.com>
2019-12-15 11:56:27 -0700

    Add parameters for instatitation of new Outfit class

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ var saveBtn = document.querySelector('#save-button');
 saveBtn.addEventListener('click', saveOutfit);
 
 function saveOutfit(){
-  var outfit= new Outfit;
+  var outfit= new Outfit(id, title, garments, background);
   outfits.push(outfit);
 }
