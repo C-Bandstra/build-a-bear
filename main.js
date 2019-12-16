3b7609985c6d7e8e09a841e2ca6462a5cf49cd50
Mandy Alford <mandyalford@gmail.com>
2019-12-16 11:17:35 -0700

    Return ID from generateID function and pass into saveOutfit function

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
+document.onload = generateId();
 var outfits =[];
-var saveBtn = document.querySelector('#save-button');
-var ID = generateId()
 
-saveBtn.addEventListener('click', saveOutfit);
+var saveBtn = document.querySelector('#save-button', saveOutfit);
 
-function saveOutfit(){
+function saveOutfit(id){
   var outfit= new Outfit(id, title, garments, background);
   outfits.push(outfit);
 }
 
 function generateId() {
-  ID= '_' + Math.random().toString(36).substr(2, 9);
+var id= Math.random().toString(36).substr(2, 9);
+console.log(id);
+return id;
 };
