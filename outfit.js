813aa974038d43c09657d842cf58dc4cd6fc3538
Mandy Alford <mandyalford@gmail.com>
2019-12-24 12:07:26 -0700

    Update garments array

--- outfit.js
+++ outfit.js
@@ -4,14 +4,14 @@ class Outfit {
     this.title= '';
     this.garments= [];
     this.background= '';
-  } 
+  }
   addGarment(newGarment){
     this.garments.push(newGarment);
-    console.log(this.garments);
+    // console.log(this.garments);
   }
   removeGarment(oldGarment){
-    console.log(this.garments);
+    // console.log(this.garments);
     this.garments.splice(this.garments.indexOf(oldGarment), 1);
-    console.log(this.garments);
+    // console.log(this.garments);
   }
 }
