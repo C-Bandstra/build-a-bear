b51d724003fad8e73460a1bace4f0f8e3acc54ee
Charlie Bandstra <bandstracharlie@gmail.com>
2020-01-08 13:53:44 -0700

    made spacing consistent

--- main.js
+++ main.js
@@ -31,16 +31,16 @@ cardContainer.addEventListener('click', removeSavedOutfit);
 
 disableSaveBtn();
 
-function disableSaveBtn(){
-  if(nameInput.value===''){
+function disableSaveBtn() {
+  if(nameInput.value === ''){
     saveBtn.disabled = true;
   } else if (nameInput.value){
     saveBtn.disabled = false;
   }
 }
 
-function createCard(){
-  if (outfit.title===nameInput.value){
+function createCard() {
+  if(outfit.title === nameInput.value){
     outfitName = nameInput.value;
     nameInput.value = '';
     resetBear(outfitName);
@@ -67,28 +67,28 @@ function createCardInHtml(displayValue) {
   </div>`);
 }
 
-function saveOutfit(){
-  outfit.title= outfitName;
+function saveOutfit() {
+  outfit.title = outfitName;
   localStorage.setItem(outfitName, JSON.stringify(outfit));
   generateId();
 }
 
-function clearAllBtns(){
+function clearAllBtns() {
   var allBtns = document.querySelectorAll('button');
-   for (var i = 0; i < allBtns.length; i++) {
+   for(var i = 0; i < allBtns.length; i++) {
        allBtns[i].classList.remove('highlight');
    }
    clearAllImages();
 }
 
-function clearAllImages(){
+function clearAllImages() {
   var allImgs = document.querySelectorAll('.hide-image');
-   for (var i = 0; i < allImgs.length; i++) {
+   for(var i = 0; i < allImgs.length; i++) {
        allImgs[i].classList.remove('show-image');
    }
 }
 
-function styleBear(style){
+function styleBear(style) {
   var item = event.target.getAttribute('id');
   var [, category] = item.split('-');
   var oldGarment = outfit.garments.find(function(garment){
@@ -96,10 +96,10 @@ function styleBear(style){
       return true;
     }
   })
-  if (oldGarment != undefined){
+  if(oldGarment != undefined) {
     outfit.removeGarment(oldGarment)
   }
-  if (!event.target.classList.contains('highlight')){
+  if(!event.target.classList.contains('highlight')) {
     outfit.addGarment(item);
   }
   pickCategory();
@@ -108,125 +108,125 @@ function styleBear(style){
   }
 }
 
-  function styleBearHats() {
-    styleBear(showHatImage);
-  }
+function styleBearHats() {
+  styleBear(showHatImage);
+}
 
-  function styleBearTops() {
-    styleBear(showTopImage);
-  }
+function styleBearTops() {
+  styleBear(showTopImage);
+}
 
-  function styleBearAccessories() {
-    styleBear(showAccessoriesImage);
-  }
+function styleBearAccessories() {
+  styleBear(showAccessoriesImage);
+}
 
-  function styleBearBackgrounds () {
-    var item = event.target.getAttribute('id');
-    outfit.background = item;
-    pickCategory();
-    if (!event.target.classList.contains('highlight')){
-      outfit.background='';
+function styleBearBackgrounds () {
+  var item = event.target.getAttribute('id');
+  outfit.background = item;
+  pickCategory();
+  if(!event.target.classList.contains('highlight')){
+    outfit.background='';
+  }
+  if(event.target.tagName == 'BUTTON') {
+    showBackgroundImage();
+  }
+}
+
+function pickCategory() {
+  if(event.target.classList.contains('hat-button')) {
+    highlightHatButton(event);
+  } else if(event.target.classList.contains('clothes-button')) {
+      highlightClothesButton(event);
+  } else if(event.target.classList.contains('accessories-button')) {
+      highlightAccessoriesButton(event);
+  } else if(event.target.classList.contains('backgrounds-button')) {
+      highlightBackgroundsButton(event);
+  }
+}
+
+function highlightHatButton() {
+  if(event.target.classList.contains('highlight')) {
+    event.target.classList.remove('highlight');
+  } else {
+    for(var i = 0; i < hatBtns.length; i++) {
+      hatBtns[i].classList.remove('highlight');
     }
-    if(event.target.tagName == 'BUTTON') {
-      showBackgroundImage();
-    }
+    event.target.classList.add('highlight');
   }
+}
 
-  function pickCategory() {
-    if(event.target.classList.contains('hat-button')) {
-      highlightHatButton(event);
-    } else if(event.target.classList.contains('clothes-button')) {
-        highlightClothesButton(event);
-    } else if(event.target.classList.contains('accessories-button')) {
-        highlightAccessoriesButton(event);
-    } else if(event.target.classList.contains('backgrounds-button')) {
-        highlightBackgroundsButton(event);
+function highlightClothesButton() {
+  if(event.target.classList.contains('highlight')) {
+    event.target.classList.remove('highlight');
+  } else {
+    for(var i = 0; i < clothesBtns.length; i++) {
+      clothesBtns[i].classList.remove('highlight');
     }
- }
-
-  function highlightHatButton() {
-    if(event.target.classList.contains('highlight')) {
-      event.target.classList.remove('highlight');
-    } else {
-      for(var i = 0; i < hatBtns.length; i++) {
-        hatBtns[i].classList.remove('highlight');
-      }
-      event.target.classList.add('highlight');
-    }
+    event.target.classList.add('highlight');
   }
+}
 
-  function highlightClothesButton() {
-    if(event.target.classList.contains('highlight')) {
-      event.target.classList.remove('highlight');
-    } else {
-      for(var i = 0; i < clothesBtns.length; i++) {
-        clothesBtns[i].classList.remove('highlight');
-      }
-      event.target.classList.add('highlight');
+function highlightAccessoriesButton() {
+  if(event.target.classList.contains('highlight')) {
+    event.target.classList.remove('highlight');
+  } else {
+    for(var i = 0; i < accessoriesBtns.length; i++) {
+    accessoriesBtns[i].classList.remove('highlight');
     }
+    event.target.classList.add('highlight');
   }
+}
 
-  function highlightAccessoriesButton() {
-    if(event.target.classList.contains('highlight')) {
-      event.target.classList.remove('highlight');
-    } else {
-      for(var i = 0; i < accessoriesBtns.length; i++) {
-      accessoriesBtns[i].classList.remove('highlight');
-      }
-      event.target.classList.add('highlight');
+function highlightBackgroundsButton() {
+  if(event.target.classList.contains('highlight')) {
+    event.target.classList.remove('highlight');
+  } else {
+    for(var i = 0; i < backgroundBtns.length; i++) {
+      backgroundBtns[i].classList.remove('highlight');
     }
+    event.target.classList.add('highlight');
   }
+}
 
-  function highlightBackgroundsButton() {
-    if(event.target.classList.contains('highlight')) {
-      event.target.classList.remove('highlight');
-    } else {
-      for(var i = 0; i < backgroundBtns.length; i++) {
-        backgroundBtns[i].classList.remove('highlight');
-      }
-      event.target.classList.add('highlight');
-    }
-  }
-
-  function showHatImage() {
-    for(var i = 0; i < hatImages.length; i++) {
-      if ((hatImages[i].classList.contains('show-image')) && (hatImages[i].classList.contains('hat'))) {
-        hatImages[i].classList.remove('show-image');
-      } else if(hatImages[i].id === event.target.dataset.id) {
-        hatImages[i].classList.add('show-image')
-      }
-    }
-  }
-
-  function showTopImage() {
-    for(var i = 0; i < topImages.length; i++) {
-      if ((topImages[i].classList.contains('show-image')) && (topImages[i].classList.contains('tops'))) {
-        topImages[i].classList.remove('show-image');
-      } else if(topImages[i].id === event.target.dataset.id) {
-        topImages[i].classList.add('show-image')
-      }
+function showHatImage() {
+  for(var i = 0; i < hatImages.length; i++) {
+    if((hatImages[i].classList.contains('show-image')) && (hatImages[i].classList.contains('hat'))) {
+      hatImages[i].classList.remove('show-image');
+    } else if(hatImages[i].id === event.target.dataset.id) {
+      hatImages[i].classList.add('show-image');
     }
   }
+}
 
-  function showAccessoriesImage() {
-    for(var i = 0; i < accessoriesImages.length; i++) {
-      if ((accessoriesImages[i].classList.contains('show-image')) && (accessoriesImages[i].classList.contains('accessory'))) {
-        accessoriesImages[i].classList.remove('show-image');
-      } else if(accessoriesImages[i].id === event.target.dataset.id) {
-        accessoriesImages[i].classList.add('show-image')
-      }
+function showTopImage() {
+  for(var i = 0; i < topImages.length; i++) {
+    if((topImages[i].classList.contains('show-image')) && (topImages[i].classList.contains('tops'))) {
+      topImages[i].classList.remove('show-image');
+    } else if(topImages[i].id === event.target.dataset.id) {
+      topImages[i].classList.add('show-image');
     }
   }
+}
 
-  function showBackgroundImage() {
-    for(var i = 0; i < backgroundImages.length; i++) {
-      if ((backgroundImages[i].classList.contains('show-image')) && (backgroundImages[i].classList.contains('bckgrnd'))) {
-        backgroundImages[i].classList.remove('show-image');
-      } else if(backgroundImages[i].id === event.target.dataset.id) {
-        backgroundImages[i].classList.add('show-image')
-      }
+function showAccessoriesImage() {
+  for(var i = 0; i < accessoriesImages.length; i++) {
+    if((accessoriesImages[i].classList.contains('show-image')) && (accessoriesImages[i].classList.contains('accessory'))) {
+      accessoriesImages[i].classList.remove('show-image');
+    } else if(accessoriesImages[i].id === event.target.dataset.id) {
+      accessoriesImages[i].classList.add('show-image');
     }
   }
+}
+
+function showBackgroundImage() {
+  for(var i = 0; i < backgroundImages.length; i++) {
+    if((backgroundImages[i].classList.contains('show-image')) && (backgroundImages[i].classList.contains('bckgrnd'))) {
+      backgroundImages[i].classList.remove('show-image');
+    } else if(backgroundImages[i].id === event.target.dataset.id) {
+      backgroundImages[i].classList.add('show-image');
+    }
+  }
+}
 
 // function highlightButton(){
   // var activeButton = event.target;
@@ -281,7 +281,7 @@ function loadSavedOutfits(){
 //maybe rename to handleCardClick
 //remo
 function removeSavedOutfit(event){
-  if (event.target.classList.contains('close-icon')){
+  if(event.target.classList.contains('close-icon')){
 
       event.target.parentElement.remove();
       localStorage.removeItem(event.target.parentElement.innerText.trim());
@@ -289,7 +289,7 @@ function removeSavedOutfit(event){
       clearAllBtns();
     }
 
-  else if (event.target.classList.contains('card-container')) {
+  else if(event.target.classList.contains('card-container')) {
       return false;
   }
      else {
@@ -321,7 +321,7 @@ function populateInput(event) {
     for(let i = 0, len = values.length; i < len; i++) {
     var item = JSON.parse(values[i]);
     debugger
-    if (item.title === outfitTitle) {
+    if(item.title === outfitTitle) {
       bearType = item;
        }
      }
@@ -341,5 +341,5 @@ function populateInput(event) {
 }
 
 function resetID(bearType){
-  outfit= (new Outfit(bearType.id, bearType.title, bearType.garments, bearType.background));
+  outfit = (new Outfit(bearType.id, bearType.title, bearType.garments, bearType.background));
 }
